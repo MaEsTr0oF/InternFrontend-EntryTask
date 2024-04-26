2bc1b9b07ea4f7dce99bd751b398eb56daa24926
MaEsTr0oF <114798814+MaEsTr0oF@users.noreply.github.com>
2024-04-26 08:47:00 +0300

    task2

--- task_2/src/App.svelte
+++ task_2/src/App.svelte
@@ -1,34 +1,18 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import Input from './lib/Counter.svelte'
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  
+  <h1>Конвертор валют</h1>
+    <Input />
 </main>
 
 <style>
+  .npm{
+    display: flex;
+    justify-content: space-between;
+  }
   .logo {
     height: 6em;
     padding: 1.5em;
--- task_2/src/lib/Counter.svelte
+++ task_2/src/lib/Counter.svelte
@@ -1,10 +1,47 @@
 <script lang="ts">
-  let count: number = 0
-  const increment = () => {
-    count += 1
-  }
+  let flag=false;
+  let name1;
+  let name2;
+  let valu=10;
+  const refresh=()=>{
+    if(flag==false){
+      flag=true;
+    }
+    else{
+      flag=false;
+    }
+  };
+  const finall=()=>{
+    if (flag==false){
+      name2=name1*valu;
+    }
+    else{
+      name1=name2*valu;
+    }
+  };
 </script>
-
-<button on:click={increment}>
-  count is {count}
-</button>
+<div class="inp">
+  <div class="inp__1">
+    <input type="text" name="message" class="inpt__1"  bind:value={name1}/>
+  </div>
+  <div class="inp__but">
+    <button class="fin" id="final" on:click={finall}>конвертировать</button>
+    <button class="refre"id="refresh" on:click={refresh}><img src="src\папка\image-3.png" alt=""></button>
+  </div>
+  <div class="inp__1">
+    <input type="text" class="inpt__1" id="input2" bind:value={name2}>
+  </div>
+</div>
+<style>
+  .inp{
+    display: flex;
+    justify-content: space-between;
+    gap:30px;
+  }
+  .inp__but{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    gap: 30px;
+  }
+</style>
